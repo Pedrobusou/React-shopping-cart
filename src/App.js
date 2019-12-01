37293ad2c3ae1dad388fe67d6276a4ae5b119957
Pedro <pedrobusou@gmail.com>
2019-12-01 21:54:59 +0100

    Multiple statelessFunctionalComponents Passing properties

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, {Fragment} from 'react';
 import EmployeeClassComponent from './components/EmployeeClass';
 import EmployeeFunctionalComponent from './components/EmployeeFunctional';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
+  const date = new Date().getFullYear();
+
   return (
     <Fragment>
-      <EmployeeClassComponent />
-      <EmployeeFunctionalComponent />
+      {/* <EmployeeClassComponent />
+      <EmployeeFunctionalComponent /> */}
+
+      <Header title="Online shop" />
+      <Footer date={date} />
     </Fragment>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = props => (
+  <footer>
+    <p>All rights reserved &copy;{props.date}</p>
+  </footer>
+);
+
+export default Footer;
