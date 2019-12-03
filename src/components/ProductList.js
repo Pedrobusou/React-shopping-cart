aeb96e63e74cfe62a3e5a84adf31546bb6b46fe4
Pedro <pedrobusou@gmail.com>
2019-12-03 22:22:21 +0100

    Lifecycle basics

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,23 +1,46 @@
 import React, {Component, Fragment} from 'react';
 import Product from './Product';
+import {timeout} from 'q';
 
 class ProductList extends Component {
   state = {
-    products: [
-      {id: 0, name: 'React tee', price: 10},
-      {id: 1, name: 'Git Trousers', price: 20},
-      {id: 2, name: 'Angular tee', price: 15}
-    ]
+    products: []
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [
+        {id: 0, name: 'React tee', price: 10},
+        {id: 1, name: 'Git Trousers', price: 20},
+        {id: 2, name: 'Angular tee', price: 15}
+      ]
+    };
+
+    setTimeout(() => {
+      this.setState({
+        products: [{id: 3, name: 'Angular gloves', price: 5}]
+      });
+    }, 2000);
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate');
+    console.log({prevProps, prevState});
+  }
+
   render() {
-    const products = this.state.products;
-    console.log(products);
+    console.log('render');
 
     return (
       <Fragment>
         <h1>Product list</h1>
-        {products.map(product => (
+        {this.state.products.map(product => (
           <Product key={product.id} product={product} />
         ))}
       </Fragment>
