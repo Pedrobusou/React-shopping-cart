62b21b796fb2a8e45b70708ddb6a3dddb8fb82de
Pedro <pedrobusou@gmail.com>
2020-02-19 20:18:37 +0100

    Destructure product. Refactor Product. Add simple click functionality

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 const Product = ({product}) => {
+  const {id, name, price} = product;
+
+  const addProductToCart = id => {
+    console.log('addProductToCart', id);
+  };
+
   return (
-    <p className="product__name">
-      {product.name}.-{product.price}€
-    </p>
+    <div className="product">
+      <h2 className="product__name">{name}</h2>
+      <p className="product__price">{price}€</p>
+      <button type="button" onClick={() => addProductToCart(id)}>
+        Add to cart
+      </button>
+    </div>
   );
 };
 
