5b2f4013abef82953a8a7e49c56ef9c4546008f8
Pedro <pedrobusou@gmail.com>
2020-02-19 20:34:11 +0100

    Pass cart and setCart down to Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const Product = ({product}) => {
-  const {id, name, price} = product;
+const Product = ({product, cart, setCart}) => {
+  const {name, price} = product;
 
-  const addProductToCart = id => {
-    console.log('addProductToCart', id);
+  const addProductToCart = product => {
+    console.log(product);
   };
 
   return (
     <div className="product">
       <h2 className="product__name">{name}</h2>
       <p className="product__price">{price}€</p>
-      <button type="button" onClick={() => addProductToCart(id)}>
+      <button type="button" onClick={() => addProductToCart(product)}>
         Add to cart
       </button>
     </div>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,17 @@
 import React, {Fragment} from 'react';
 import Product from './Product';
 
-const ProductList = ({products}) => {
+const ProductList = ({products, cart, setCart}) => {
   return (
     <Fragment>
       <h1>Product list</h1>
       {products.map(product => (
-        <Product key={product.id} product={product} />
+        <Product
+          key={product.id}
+          product={product}
+          cart={cart}
+          setCart={setCart}
+        />
       ))}
     </Fragment>
   );
